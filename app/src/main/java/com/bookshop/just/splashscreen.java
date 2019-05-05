7b18992a5f5c3585a9a64a582fd2faddf98d8aab
abdurrahman <rahman.just16@gmail.com>
2019-05-06 04:35:48 +0600

    Final app

---
+++ app/src/main/java/com/bookshop/just/splashscreen.java
@@ -0,0 +1,28 @@
+package com.bookshop.just;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        Thread my= new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(3000);
+                    Intent i= new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        my.start();
+    }
+}
